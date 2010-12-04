74660aad399e311dba9b8948ee87f33b7a9fc470
dwalton76 <dwalton76@gmail.com>
2010-12-04 08:50:51 -0500

    Added a wrapper script for running the script one page at a time.  This is a temporary fix to reduce the impact of the memory leak

---
+++ wrapper.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# This is a wrapper script for running geni-automerge.pl on one page at a time.
+# This is a temporary fix until we can find the mem leak(s) that cause the script
+# to hold hundreds of megs of memory when you run it for more than a few hours.
+#
+# The api limit is set to 10.  I did this because I typically run 4 copies of
+# the script at once and our overall "api limit" is 40.
+#
+# The syntax is "./wrapper.pl X Y" where X is your -rb and Y is your -re
+#
+use strict;
+my $rb = $ARGV[0];
+my $re = $ARGV[1];
+my $api = 10;
+
+for (my $i = $re; $i >= $rb; $i--) {
+	print "./geni-automerge.pl -tcs -mlt -api $api -rb $i -re $i\n";
+	system "./geni-automerge.pl -tcs -mlt -api $api -rb $i -re $i";
+}
