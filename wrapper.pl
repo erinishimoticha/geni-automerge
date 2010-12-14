160f8411795f1664bd97b54170fff2d47ed1335d
dwalton76 <dwalton76@gmail.com>
2010-12-14 01:34:54 -0500

    wrapper.pl fixes

--- wrapper.pl
+++ wrapper.pl
@@ -10,18 +10,29 @@
 # The syntax is "./wrapper.pl X Y" where X is your -rb and Y is your -re
 #
 use strict;
-my $rb = $ARGV[0];
-my $re = $ARGV[1];
-my $mode = $ARGV[2];
-my $api = $ARGV[3]; 
+my $rb = "";
+my $re = "";
+my $mode = "";
+my $api = "";
 
-if ($mode ne "-tcs" && $mode ne "-pms") {
-	die("ERROR: mode must be either -tcs or -pms, you entered '$mode'\n\n");
+for (my $i = 0; $i <= $#ARGV; $i++) {
+	if ($ARGV[$i] eq "-api") {
+		$api = $ARGV[++$i];
+	} elsif ($ARGV[$i] eq "-pms") {
+		$mode = "-pms";
+	} elsif ($ARGV[$i] eq "-tcs") {
+		$mode = "-tcs";
+	} elsif ($ARGV[$i] eq "-rb") {
+		$rb = $ARGV[++$i];
+	} elsif ($ARGV[$i] eq "-re") {
+		$re = $ARGV[++$i];
+	}
 }
 
-if (!$api) {
-	die("ERROR: you must specify an api value via '-api X'\n\n");
-}
+die("ERROR: mode must be either -tcs or -pms, you entered '$mode'\n\n") if ($mode ne "-tcs" && $mode ne "-pms");
+die("ERROR: you must specify an api value via '-api X'\n\n") if ($api !~ /^\d+$/);
+die("ERROR: you must specify a rb value via '-rb X'\n\n") if ($rb !~ /^\d+$/);
+die("ERROR: you must specify a re value via '-re X'\n\n") if ($re !~ /^\d+$/);
 
 for (my $i = $re; $i >= $rb; $i--) {
 	if ($mode eq "-tcs") {
@@ -37,6 +48,6 @@ for (my $i = $re; $i >= $rb; $i--) {
 
 print "$re -> $rb\n";
 for (my $i = $re; $i >= $rb; $i--) {
-	print "./geni-automerge.pl -tcs -mlt -api $api -rb $i -re $i\n";
-	system "./geni-automerge.pl -tcs -mlt -api $api -rb $i -re $i";
+	print "./geni-automerge.pl $mode -all -mlt -api $api -rb $i -re $i\n";
+	system "./geni-automerge.pl $mode -all -mlt -api $api -rb $i -re $i";
 }
